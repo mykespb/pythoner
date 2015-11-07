a122a7b0a91e306eca1f9367b2637472d98303e2
mykespb <mykespb@gmail.com>
2015-11-07 04:08:04 +0300

    checks what integers have predefined nodes: -5..256

---
+++ mk-ckeck-ints.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# mk-check-ints.py (c) myke 2015-11-07
+# check first integer predefined refs
+
+for i in range (-10, 300):
+    a = i+0
+    b = i+0
+    if a is b:
+        print (i, "equals")
+    else:
+        print (i, "not equals")
+
+#~ -10 not equals
+#~ -9 not equals
+#~ -8 not equals
+#~ -7 not equals
+#~ -6 not equals
+#~ -5 equals
+#~ -4 equals
+#~ -3 equals
+#~ -2 equals
+#~ -1 equals
+#~ 0 equals
+#~ 1 equals
+#~ 2 equals
+#~ 3 equals
+#~ 4 equals
+#~ 5 equals
+#~ 6 equals
+#~ 7 equals
+#~ 8 equals
+#~ 9 equals
+#~ 10 equals
+#~ 11 equals
+#~ 12 equals
+#~ 13 equals
+#~ 14 equals
+#~ 15 equals
+#~ 16 equals
+#~ 17 equals
+#~ 18 equals
+#~ 19 equals
+#~ 20 equals
+#~ 21 equals
+#~ 22 equals
+#~ 23 equals
+#~ 24 equals
+#~ 25 equals
+#~ 26 equals
+#~ 27 equals
+#~ 28 equals
+#~ 29 equals
+#~ 30 equals
+#~ 31 equals
+#~ 32 equals
+#~ 33 equals
+#~ 34 equals
+#~ 35 equals
+#~ 36 equals
+#~ 37 equals
+#~ 38 equals
+#~ 39 equals
+#~ 40 equals
+#~ 41 equals
+#~ 42 equals
+#~ 43 equals
+#~ 44 equals
+#~ 45 equals
+#~ 46 equals
+#~ 47 equals
+#~ 48 equals
+#~ 49 equals
+#~ 50 equals
+#~ 51 equals
+#~ 52 equals
+#~ 53 equals
+#~ 54 equals
+#~ 55 equals
+#~ 56 equals
+#~ 57 equals
+#~ 58 equals
+#~ 59 equals
+#~ 60 equals
+#~ 61 equals
+#~ 62 equals
+#~ 63 equals
+#~ 64 equals
+#~ 65 equals
+#~ 66 equals
+#~ 67 equals
+#~ 68 equals
+#~ 69 equals
+#~ 70 equals
+#~ 71 equals
+#~ 72 equals
+#~ 73 equals
+#~ 74 equals
+#~ 75 equals
+#~ 76 equals
+#~ 77 equals
+#~ 78 equals
+#~ 79 equals
+#~ 80 equals
+#~ 81 equals
+#~ 82 equals
+#~ 83 equals
+#~ 84 equals
+#~ 85 equals
+#~ 86 equals
+#~ 87 equals
+#~ 88 equals
+#~ 89 equals
+#~ 90 equals
+#~ 91 equals
+#~ 92 equals
+#~ 93 equals
+#~ 94 equals
+#~ 95 equals
+#~ 96 equals
+#~ 97 equals
+#~ 98 equals
+#~ 99 equals
+#~ 100 equals
+#~ 101 equals
+#~ 102 equals
+#~ 103 equals
+#~ 104 equals
+#~ 105 equals
+#~ 106 equals
+#~ 107 equals
+#~ 108 equals
+#~ 109 equals
+#~ 110 equals
+#~ 111 equals
+#~ 112 equals
+#~ 113 equals
+#~ 114 equals
+#~ 115 equals
+#~ 116 equals
+#~ 117 equals
+#~ 118 equals
+#~ 119 equals
+#~ 120 equals
+#~ 121 equals
+#~ 122 equals
+#~ 123 equals
+#~ 124 equals
+#~ 125 equals
+#~ 126 equals
+#~ 127 equals
+#~ 128 equals
+#~ 129 equals
+#~ 130 equals
+#~ 131 equals
+#~ 132 equals
+#~ 133 equals
+#~ 134 equals
+#~ 135 equals
+#~ 136 equals
+#~ 137 equals
+#~ 138 equals
+#~ 139 equals
+#~ 140 equals
+#~ 141 equals
+#~ 142 equals
+#~ 143 equals
+#~ 144 equals
+#~ 145 equals
+#~ 146 equals
+#~ 147 equals
+#~ 148 equals
+#~ 149 equals
+#~ 150 equals
+#~ 151 equals
+#~ 152 equals
+#~ 153 equals
+#~ 154 equals
+#~ 155 equals
+#~ 156 equals
+#~ 157 equals
+#~ 158 equals
+#~ 159 equals
+#~ 160 equals
+#~ 161 equals
+#~ 162 equals
+#~ 163 equals
+#~ 164 equals
+#~ 165 equals
+#~ 166 equals
+#~ 167 equals
+#~ 168 equals
+#~ 169 equals
+#~ 170 equals
+#~ 171 equals
+#~ 172 equals
+#~ 173 equals
+#~ 174 equals
+#~ 175 equals
+#~ 176 equals
+#~ 177 equals
+#~ 178 equals
+#~ 179 equals
+#~ 180 equals
+#~ 181 equals
+#~ 182 equals
+#~ 183 equals
+#~ 184 equals
+#~ 185 equals
+#~ 186 equals
+#~ 187 equals
+#~ 188 equals
+#~ 189 equals
+#~ 190 equals
+#~ 191 equals
+#~ 192 equals
+#~ 193 equals
+#~ 194 equals
+#~ 195 equals
+#~ 196 equals
+#~ 197 equals
+#~ 198 equals
+#~ 199 equals
+#~ 200 equals
+#~ 201 equals
+#~ 202 equals
+#~ 203 equals
+#~ 204 equals
+#~ 205 equals
+#~ 206 equals
+#~ 207 equals
+#~ 208 equals
+#~ 209 equals
+#~ 210 equals
+#~ 211 equals
+#~ 212 equals
+#~ 213 equals
+#~ 214 equals
+#~ 215 equals
+#~ 216 equals
+#~ 217 equals
+#~ 218 equals
+#~ 219 equals
+#~ 220 equals
+#~ 221 equals
+#~ 222 equals
+#~ 223 equals
+#~ 224 equals
+#~ 225 equals
+#~ 226 equals
+#~ 227 equals
+#~ 228 equals
+#~ 229 equals
+#~ 230 equals
+#~ 231 equals
+#~ 232 equals
+#~ 233 equals
+#~ 234 equals
+#~ 235 equals
+#~ 236 equals
+#~ 237 equals
+#~ 238 equals
+#~ 239 equals
+#~ 240 equals
+#~ 241 equals
+#~ 242 equals
+#~ 243 equals
+#~ 244 equals
+#~ 245 equals
+#~ 246 equals
+#~ 247 equals
+#~ 248 equals
+#~ 249 equals
+#~ 250 equals
+#~ 251 equals
+#~ 252 equals
+#~ 253 equals
+#~ 254 equals
+#~ 255 equals
+#~ 256 equals
+#~ 257 not equals
+#~ 258 not equals
+#~ 259 not equals
+#~ 260 not equals
+#~ 261 not equals
+#~ 262 not equals
+#~ 263 not equals
+#~ 264 not equals
+#~ 265 not equals
+#~ 266 not equals
+#~ 267 not equals
+#~ 268 not equals
+#~ 269 not equals
+#~ 270 not equals
+#~ 271 not equals
+#~ 272 not equals
+#~ 273 not equals
+#~ 274 not equals
+#~ 275 not equals
+#~ 276 not equals
+#~ 277 not equals
+#~ 278 not equals
+#~ 279 not equals
+#~ 280 not equals
+#~ 281 not equals
+#~ 282 not equals
+#~ 283 not equals
+#~ 284 not equals
+#~ 285 not equals
+#~ 286 not equals
+#~ 287 not equals
+#~ 288 not equals
+#~ 289 not equals
+#~ 290 not equals
+#~ 291 not equals
+#~ 292 not equals
+#~ 293 not equals
+#~ 294 not equals
+#~ 295 not equals
+#~ 296 not equals
+#~ 297 not equals
+#~ 298 not equals
+#~ 299 not equals
