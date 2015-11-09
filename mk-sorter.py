bcf6dd2e5eebbc7f37db4781f6515c8d3211b094
mykespb <mykespb@gmail.com>
2015-11-09 15:45:28 +0300

    classic sorting algos in py

---
+++ mk-sorter.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# myke mk-sorter.py 2015-11-09 1.1
+# classic selection and bubble sortings
+
+import random
+
+TIMES = 10
+SIZE  = 10
+RANGE = 10
+
+print ("hello sorter")
+
+# -----------------------------------------------
+
+def selsort (a):
+    """selection sorter"""
+    for i in range(len(a)-1):
+        for j in range(i+1, len(a)):
+            if a[i] > a[j]:
+                a[i], a[j] = a[j], a[i]
+    return a
+
+# -----------------------------------------------
+
+def bubsort (a):
+    """bubble sorter"""
+
+    need = True
+    while need:
+        need = False
+        for i in range(len(a)-1):
+            if a[i] > a[i+1]:
+                a[i], a[i+1] = a[i+1], a[i]
+                need = True
+    return a
+
+# -----------------------------------------------
+qs = bubsort
+
+# -----------------------------------------------
+def main ():
+    """ dispatcher: tests make and sort """
+
+    for i in range(TIMES):
+        sa = [random.randint(-RANGE, RANGE) for e in range(SIZE)]
+        print (sa, "-->", qs (sa))
+
+main()
+
+# ===============================================
