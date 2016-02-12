ffbc76a04cdc2731b49c451732e3ea73fc30a422
mykespb <mykespb@gmail.com>
2016-02-13 00:31:41 +0300

    return tuple of tuples: better

--- mk-anagram.py
+++ mk-anagram.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# M.Kolodin mk-anagram.py 2016-02-12 2016-02-12 1.0
+# M.Kolodin mk-anagram.py 2016-02-12 2016-02-12 1.2
 # find all anagrams in given text
+# return tuple of tuples with anagramix words
 
 text = """
 123 345 231
 ethic thetic tethic
 удача учада чаадаев овин вино нави винна
+tuli tulli tuuli tuulli util little litu
 """
 
 def base (t):
@@ -16,18 +18,18 @@ def base (t):
 def trovu (t):
     """ find all anagrams in given list of words """
     res = {}
-    for w in t:
+    for w in t.split():
         ind = base(w)
         if ind in res:
             res [ind] += [w]
         else:
             res [ind] = [w]
     res = [res[x] for x in res if len(res[x])>1]
-    res = [sorted(x) for x in res]
-    return sorted(res)
+    res = [tuple(sorted(x)) for x in res]
+    return tuple(sorted(res))
 
 def main(args):
-    print (trovu (text.split()))
+    print (trovu (text))
     return 0
 
 if __name__ == '__main__':
