0a13b5b3b468149a5c15124b87e0279727168b91
mykespb <mykespb@gmail.com>
2016-03-31 20:57:00 +0300

    cmd-words

--- mtm.py
+++ mtm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # mtm = myke's 'time manager
-# 2016-03-31 1.9
+# 2016-03-31 1.10
 
 # use:
 # mkm <cmd> params
@@ -15,12 +15,13 @@
 
 import sys, datetime, os
 
-version = "1.8"
+version = "1.10"
 fout = os.getenv('MTM', os.getcwd() + '/mtm.log')
 
 dt = str(datetime.datetime.now())[:-7]
 
-cmdlist = "? help on off out up down home bed slept read tv inet chat busy prog ate study watch tea away stat report".split()
+cmdlist = "? help on off out up down home bed slept read tv inet chat busy prog ate study watch tea away stat walk sport travel report".split()
+cmdlist.sort()
 
 def main(args):
     print ("This is MTM myke's Time Management routine ver. {}" .format (version))
