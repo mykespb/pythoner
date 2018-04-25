62a6d4859c79164c932585aeaca9c360cc11b988
mykespb <mykespb@gmail.com>
2018-04-25 23:07:01 +0300

    foobar task added

---
+++ foobar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  foobar.py
+#  Write a program that prints the numbers from 1 to 100. But for multiples of three print “Foo” instead of the number and for the multiples of five print “Bar”. For numbers which are multiples of both three and five print “FooBar”.
+
+def main():
+
+    for n in range(1, 101):
+        print (
+            "FooBar" if n%15 == 0 else
+            "Foo" if n%3 == 0 else
+            "Bar" if n%5 == 0 else n
+            , ", ", end="")
+
+    return 0
+
+if __name__ == '__main__':
+    main()
