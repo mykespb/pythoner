93d122919d4a3e9d5e793f05cccebb722152ceac
mykespb <mykespb@gmail.com>
2018-04-26 00:02:20 +0300

    foobar task added with lines

--- foobar.py
+++ foobar.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  foobar.py
+#  foobar.py 1.2
 #  Write a program that prints the numbers from 1 to 100. But for multiples of three print “Foo” instead of the number and for the multiples of five print “Bar”. For numbers which are multiples of both three and five print “FooBar”.
 
 def main():
@@ -11,7 +11,8 @@ def main():
             "FooBar" if n%15 == 0 else
             "Foo" if n%3 == 0 else
             "Bar" if n%5 == 0 else n
-            , ", ", end="")
+            , ", ",
+            end="\n" if n%10 == 0 else "")
 
     return 0
 
