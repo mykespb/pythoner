c0894d3c14b8273364454dfa13c94311578ff698
mykespb <mykespb@gmail.com>
2018-05-31 17:21:28 +0300

    update for diverse usage

--- mk-1strecurring.py
+++ mk-1strecurring.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-# (C) Mikhail Kolodin, 2018, ver. 1.0
+# (C) Mikhail Kolodin, 2018, ver. 2018-05-31 1.1
 # class ic test task: find 1st recurring character in a string
 
 import random
 import string
 
 MINSIZE = 1       # min size of test string
-MAXSIZE = 9       # its max size
+MAXSIZE = 19      # its max size
 TESTS = 10        # no of tests
 
 alf = string.ascii_uppercase   # test alphabet
@@ -14,15 +14,17 @@ alf = string.ascii_uppercase   # test alphabet
 arr = []
 size = 0
 
+
 def prepare():
     """organize tests"""
     global arr, size
+
     size = random.randint(MINSIZE, MAXSIZE)
     arr = "".join([random.choice(alf) for i in range(size)])
 
+
 def solve():
-    """find char"""
-    global arr
+    """find char, reusable function"""
 
     found = ""
 
@@ -32,15 +34,23 @@ def solve():
         else:
             found += c
     else:
-        return "None"
+        return ""
+
+
+def show():
+    """find and show char, function to show result only"""
+
+    c = solve()
+    return c if c else "None"
 
 
 def main():
     """run all"""
-    global arr, szie
 
     for test in range(TESTS):
         prepare()
-        print ("test =", test, ", size =", size, ", arr =", arr.ljust(MAXSIZE), ", found recurrent:", solve())
+        print ("test =", test, ", size = %2d" % (size), ", arr =", arr.ljust(MAXSIZE), ", found recurrent:", show())
 
-main()
+
+if __name__ == "__main__":
+    main()
