c26750ee54092c09b6648e2e8ae9ad2aedb65e88
mykespb <mykespb@gmail.com>
2018-06-02 13:19:10 +0300

    complete naive slow solution

--- mk-maxrecthist.py
+++ mk-maxrecthist.py
@@ -9,7 +9,7 @@
 #  (but solving method is not from there)
 #
 #  (C) Mikhail Kolodin, 2018
-#  ver. 2018-06-02 0.2
+#  ver. 2018-06-02 1.0 naive slow solution
 
 import random
 
@@ -26,16 +26,64 @@ hist = ()          # histoghram, global
 def generate():
     """generate histogram"""
     global hist, HOR, VERT
+
     HOR = random.randint(1, MAX_HOR)
     VERT = random.randint(MAX_VERT//2, MAX_VERT)
     hist = [random.randint(0, VERT) for _ in range(HOR)]
-    print(hist)
+
+    print(hist, "\n")
+    pretty(hist)
+
+
+def pretty(h):
+    """print pretty histogram"""
+
+    for j in range(VERT, 0, -1):
+        for i in range(HOR):
+            print("* " if hist[i] >= j else "  ", end="")
+        print()
+
+    for i in range(HOR):
+        print(i, end=" ")
+    print("\n")
 
 
 def solve():
     """solve task: find max area"""
-    #TODO
-    pass
+    # bests
+    best_start = 0
+    best_area = 0
+    best_top = 0
+    best_end = 0
+
+    for si in range(HOR):
+        if hist[si] == 0:
+            continue
+        for sj in range(1, hist[si]+1):
+            area, bend = calc_area(si, sj)
+            if area > best_area:
+                best_start = si
+                best_end = bend
+                best_area = area
+                best_top = sj
+                print("debug: found better: start=", best_start, ", end=", best_end, ", top=", best_top, ", area=", best_area)
+
+    print("\nsolution: start=", best_start, ", end=", best_end, ", top=", best_top, ", area=", best_area)
+
+
+def calc_area(si, sj):
+    """calc area of rectangle in histogram starting in column si and having height sj"""
+
+    area = sj
+    bend = si
+    for ei in range(si+1, HOR):
+        if hist[ei] >= sj:
+            area += sj
+            bend = ei
+        else:
+            break
+
+    return area, bend
 
 
 def main(args):
