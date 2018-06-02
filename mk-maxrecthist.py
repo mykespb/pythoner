2989a107a7309c23b1a3d316e24b0e061fa9345e
mykespb <mykespb@gmail.com>
2018-06-02 11:05:32 +0300

    new task: histogram max area finder

---
+++ mk-maxrecthist.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  mk-maxrecthist.py
+#  find rectangle in histogram with max square
+#
+#  See also: https://www.youtube.com/watch?v=VNbkzsnllsU
+#  Coding Interview Problem: Largest Rectangle in a Histogram
+#  (but solving method is not from there)
+#
+#  (C) Mikhail Kolodin, 2018
+#  ver. 2018-06-02 0.1
+
+NO_TESTS = 1       # # of tests
+MAX_HOR = 10       # max # of columnes
+MAX_VERT = 10      # max column height
+
+HOR = 0            # sizes for this run
+VERT = 0
+
+hist = ()          # histoghram, global
+
+
+def generate():
+    """generate histogram"""
+    pass
+
+
+def solve():
+    """solve task: find max area"""
+    pass
+
+
+def main():
+    """runner"""
+    for test in range(NO_TESTS):
+        generate()
+        solve()
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
