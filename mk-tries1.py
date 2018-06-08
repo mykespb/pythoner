b459ccfa4b22271e81a1956e361aad382bc007ea
mykespb <mykespb@gmail.com>
2018-06-08 14:55:21 +0300

    change data class

--- mk-tries1.py
+++ mk-tries1.py
@@ -14,66 +14,89 @@
 class Trie:
     """class for Trie"""
 
-    def __init__(self, data=None, status='start'):
+    def __init__(self, data=None):
         """create new node"""
-        self.data ='*'
         self.links = {}
-        self.status = status
         if data:
             self.add(data)
 
     def add(self, data):
         """add word to trie"""
-        #TODO
-        pass
+        if not data: return
 
-    def __repr__(self):
-        """autoshow me"""
-        out = "<Trie '" + self.data + "' "
-        if self.status != 'stop':
-            for link in self.links:
-                print(link)
-        out += "> "
-        return out
+        c = data[0]
+        data = data[1:]
 
-    def __str__(self):
-        """printable show"""
-        out = ''
-        if self.status != 'stop':
-            if out:
-                out += ', ' + self.data
+        if c in self.links:
+            to = self.links[c]
+            if not to and not data:
+                return
+            if not data:
+                self.links[c] = Trie()
+                return
+            if not to:
+                self.links[c] = Trie(data)
+                return
+            to.add(data)
+            return
+        else:
+            if data:
+                self.links[c] = Trie(data)
+                return
             else:
-                out += self.data
-            for link in self.links:
-                print(link)
-        return out
+                self.links[c] = {}
+                return
+
 
     def has(self, data):
         """say if data is in trie"""
         #TODO
         pass
 
+    def __str__(self):
+        """printable show"""
+        if not self.links:
+            return '()'
+        out = '(' + self.allstrings() + ')'
+        return out
+
+    def allstrings(self):
+        """give all strings, separated by commas"""
+        if not self.links:
+            return ""
+        out = ''
+        for c, link in self.links.items():
+            if c:
+                out += c
+                if link:
+                    out += link.allstrings()
+                else:
+                    out += ', '
+            else:
+                out += ', '
+        return out
+
+
 def main(args):
 
     t = Trie()
+
+    t.add("A")
     print(t)
-    print(t.__repr__())
-
-    # ~ t.add("CALL")
-    # ~ t.add("CITY")
-    # ~ t.add("CAT")
-    # ~ print(t)
-
-    # ~ test_set = "CASE", "CITY", "FINAL", "CAT"
-
-    # ~ for test in test_set:
-        # ~ print("test if", test, "is in trie gives", t.has(test))
-
-    # ~ t = Trie("CUTTY")
-    # ~ print(t)
-
-    # ~ t.add("FINITA")
-    # ~ print(t)
+    t.add("TO")
+    print(t)
+    t.add("TAR")
+    print(t)
+    t.add("TARK")
+    print(t)
+    t.add("TOOLS")
+    print(t)
+    t.add("TOOL")
+    print(t)
+    t.add("CAT")
+    print(t)
+    t.add("AN")
+    print(t)
 
     return 0
 
