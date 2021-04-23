80a3ad4967d614fb9619a523d6cc269b92cf868d
mykespb <mykespb@gmail.com>
2021-04-23 10:33:10 +0300

    таблица умножения раз

---
+++ mk-mults.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# таблица умножения
+
+def tabmult2():
+    for i in range(1, 10):
+        for j in range(1, 10):
+            print ( "%3d" % (i*j,), end="")
+        print()
+
+def main(args):
+    tabmult2()
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
