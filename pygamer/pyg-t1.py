d2a24e72debafdb834d5063d8e3579ef27fc6b6c
mykespb <mykespb@gmail.com>
2021-08-03 22:32:06 +0300

    delete all

---
+++ pygamer/pyg-t1.py
@@ -0,0 +1,14 @@
+# myke - test pygame 2021-08-03 1.0
+
+import pygame
+from pygame.locals import *
+
+# ti = pygame.init()
+# print(ti)
+# f = pygame.font.init()
+# print(f)
+
+pygame.init()
+game_display = pygame.display.set_mode((800, 600))
+
+
