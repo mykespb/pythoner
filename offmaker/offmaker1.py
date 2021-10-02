dc576b4edd40bbdd3d26d1a7c2643e523d9c778f
mykespb <mykespb@gmail.com>
2021-10-03 01:33:04 +0300

    done with excel, starting with docx

--- offmaker/offmaker1.py
+++ offmaker/offmaker1.py
@@ -8,6 +8,7 @@
 
 # ~ import xlrd
 import openpyxl
+from docx import Document
 
 template  = "template.docx"
 lopersons = "lopersons.xlsx"
@@ -16,20 +17,30 @@ outdir    = "outdir"
 print ("here we start with {} as template and {} as list of persons\nresult files will be written to {} directory".format
     (template, lopersons, outdir))
 
-with open (template) as ftpl:
-    print ("file '{}' is open" . format (template))
+wb = openpyxl.load_workbook (lopersons)
+sheet = wb.active
+print (sheet)
 
-    # ~ wb = xlrd.open_workbook(lopersons)
-    # ~ sheet = wb.shhet_by_index(0)
-    # ~ sheet.cell_value(0, 0)
-    # ~ print (sheet.ncols)
+print("Rows: {}, columns: {}" . format(smr := sheet.max_row, smc := sheet.max_column))
+cols = []
+for item in sheet.iter_cols(0, smc):
+    cols.append(item[0].value)
+print(cols)
 
-    wb = openpyxl.load_workbook (lopersons)
-    sheet = wb.active
-    print (sheet)
+for i, row in enumerate (sheet.iter_rows (values_only = True)):
+    if i == 0:
+        continue
 
-    print("Rows: {}, columns: {}" . format(smr := sheet.max_row, smc := sheet.max_column))
-    cols = []
-    for item in sheet.iter_cols(0, smc):
-        cols.append(item[0].value)
-    print(cols)
+    print ("-------------------------------\nline {}" .format(i))
+    for j in range(smc):
+        print ("data {} is {}" .format(cols[j], row[j]))
+
+doc = Document(template)
+
+print (doc.paragraphs)
+
+# useful links:
+
+# https://www.marsja.se/your-guide-to-reading-excel-xlsx-files-in-python/
+# Your Guide to Reading Excel (xlsx) Files in Python
+# by Erik Marsja | Feb 16, 2020 | Programming, Python
