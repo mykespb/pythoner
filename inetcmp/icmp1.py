c50231f9646126062f1aff399e2e7c7ab0a8a427
mykespb <mykespb@gmail.com>
2021-12-31 01:15:43 +0300

    0th working version

--- inetcmp/icmp1.py
+++ inetcmp/icmp1.py
@@ -6,53 +6,66 @@
 # 2021-12-31 2021-12-31 0.1
 
 # берём страницу в инете, запоминаем фрагмент,
-# а потом повторными запусками проверяем, изменилася ли страница в указанном месте
+# а потом повторными запусками проверяем, изменилась ли страница в указанном месте
 
+import os.path
 import sys
 import lxml
 
-store = "icmp.txt"
+fstore = "icmp.tab"
 
 help = """
 This is icmp.py.
 Usage:
     icmp
         show help
-    icmp page
-        1st run:   read and store in icmp.txt
-        next runs: compare and inform if there are diifs
+    icmp site pattern
+        1st run:   read and store in icmp.tab
+        next runs: compare and inform if there are diifs.
+        note: only 1st pattern case is processed.
 """
 
 
-def inbsase(cmd):
+def inbsase(psite, ppattern):
     """
-    проверить, есть ли уже в базе такая строка
+    проверить, есть ли уже в базе такая строка.
+    если да, то вернуть значение.
+    если нет, то ''.
     """
-    ...
+    with open(fstore) as store:
+        for aline in store:
+            line = line.strip()
+            site, pattern, value = line.split()
+            if site == psite and pattern == pattern:
+                return val
+    return ""
+    
 
-def refind(cmd):
+def refind(psite, ppattern):
     """
     найти строку в инете и сравнить с сохранённой
     """
     ...
 
-def store(cmd):
+def store(psite, ppattern):
     """
     сохранить строку в базе
     """
     ...
 
-if len(sys.argv) >1:
-    cmd = sys.argv[1]
-    if cmd.startswith(('-h', 'help')):
-        print(help)
-        exit(0)
-    else:
-        if inbase(cmd):
-            refind(cmd)
+if not os.path.isfile("./" + store):
+    with open(fstore, 'w') as store:
+        store.write("site\tpattern\tvalue\n")
+
+if len(sys.argv) > 2:
+    site, pattern = sys.argv[1:3]
+
+        there = dofind(site, pattern)
+        
+        if inbase(cmd, s):
+            refind(site, pattern)
         else:
-            store(cmd)
+            store(site, pattern)
 else:
     print(help)
     exit(0)
-
