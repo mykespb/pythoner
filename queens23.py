b3d5050b766744f04a3e816deed4fe4eb2a79cab
mykespb <mykespb@gmail.com>
2026-02-27 09:56:09 +0300

    queens up

--- queens23.py
+++ queens23.py
@@ -2,20 +2,24 @@
 # -*- coding: utf-8 -*-
 
 def rc_queens(n_col, width, sol):
+    global cnt
     if len(sol) == width:
-        print(sol)
+        cnt += 1
+        print("solution %2d:" % cnt, sol)
     else:
         for n_row in range(width):
-            if (safe_queen(n_row, n_col, sol)):
+            if safe_queen(n_row, n_col, sol):
                 rc_queens(n_col+1, width, sol+[n_row])
 
 def safe_queen(new_row, new_col, sol):
     for col in range(len(sol)):
-        if (sol[col] == new_row or
-            abs(col - new_col) == abs(sol[col] - new_row)):
-                return 0
-    return 1
+        if sol[col] == new_row or abs(col - new_col) == abs(sol[col] - new_row):
+            return False
+    return True
 
-if __name__ == "__main__":
+def main():
     for n in range(8):
         rc_queens(1, 8, [n])
+
+cnt = 0
+main()
